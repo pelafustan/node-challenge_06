585660e8d7738f209cecffe2ca26de1b7386e58d
Patricio Parada <paradagpatricio@gmail.com>
2024-05-28 11:31:50 -0400

    add a needed functions and endpoints, still WIP

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const { checkCredentials, getUserData } = require('./queries');
 
 const PORT = process.env.API_PORT || 3000;
 
@@ -10,13 +11,42 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+// test endopoint to check everything is running smoothly
 app.get('/', (req, res) => {
     try {
         res.status(200).send({ url: req.originalUrl, method: req.method });
     } catch (err) {
         console.log(err);
-        res.status(err.code || 400).send({ error: err.code || 400, error: err });
+        res.status(err.code || 500).send({ error: err.code || 500, error: err });
     }
+});
+
+// post endpoint that handles user login
+app.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const token = await checkCredentials(email, password);
+        res.status(200).json({ token: token });
+    } catch (err) {
+        res.status(err.code).send(err.message);
+    }
+});
+
+// get endpoint to retrieve user's information from the db
+app.get('/user', async (req, res) => {
+    try {
+        const token = req.header('Authorization').split('Bearer ')[1];
+        const user = await getUserData(token);
+        res.status(200).json(user);
+    } catch (err) {
+        console.log(err);
+        res.status(err.code || 500).send(err.message || 'Something went wrong.');
+    }
+});
+
+// send 404 for every route/method that isn't supported
+app.use('*', (_, res) => {
+    res.status(404).send('Endpoint does not exist or method is not allowed');
 });
 
 app.listen(PORT, () => console.log(`Server up and running! PORT: ${PORT}`));
--- server/queries.js
+++ server/queries.js
@@ -1,12 +1,16 @@
 require('dotenv').config();
 
 const { Pool } = require('pg');
+const bcrypt = require('bcryptjs');
+
+const jwt = require('jsonwebtoken');
 
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_NAME = process.env.DB_NAME;
 const DB_HOST = process.env.DB_HOST;
 const DB_PORT = process.env.DB_PORT;
+const SECRET = process.env.SECRET;
 
 const pool = new Pool({
     user: DB_USER,
@@ -17,10 +21,44 @@ const pool = new Pool({
     allowExitOnIdle: true,
 });
 
-const getData = async (tableName) => {
-    const query = `SELECT * from ${tableName}`;
-    const { rows } = await pool.query(query);
-    return rows;
+// function that compare the user credentials
+const checkCredentials = async (email, password) => {
+    const query = 'SELECT password FROM users WHERE email = $1';
+
+    const { rows: [{ pass }], rowCount } = await pool.query(query, [email]);
+
+    if (!rowCount || !bcrypt.compareSync(password, pass)) {
+        throw { code: 401, message: 'Password and email do not match' };
+    }
+
+    const token = jwt.sign({ email }, SECRET, { expiresIn: '10 minutes' });
+
+    return token;
 };
 
-module.exports = { getData };
+// if we have a valid token, we return the user's information
+const getUserData = async (token) => {
+    try {
+        const { email } = jwt.verify(token, SECRET);
+
+        const query = 'SELECT * FROM users WHERE email=$1';
+        const { rows: [user], rowCount } = pool.query(query, [email]);
+
+        if (!rowCount) throw { code: 401, message: 'Unauthorized' };
+
+        return user;
+    } catch (err) {
+        throw { code: err.code, message: err.message };
+    }
+};
+
+// add new entries to users table
+const addUser = async (user) => {
+    const { email, password, role, languages } = user;
+    const pass = bcrypt.hashSync(password);
+    const query = 'INSERT INTO users (email, password, role, languages) VALUES ($1, $2, $3, $4)';
+    const { rowCount } = await pool.query(query, [email, pass, role, languages]);
+    if (!rowCount) throw { code: 500, message: 'User cannot be created!' };
+};
+
+module.exports = { getUserData, addUser, checkCredentials };
